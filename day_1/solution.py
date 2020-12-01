22655da7df543e3d5742fe9e7bed1c8054a9d1e5
johnnyflame <johnnyflamelee@gmail.com>
2020-12-01 11:49:11 +0100

    minor refactoring--putting the input parser into it's own function

--- day_1/solution.py
+++ day_1/solution.py
@@ -7,18 +7,6 @@ class ArgParser(argparse.ArgumentParser):
         # kwargs["description"], kwargs["epilog"] = full_help.split("\n\n", 1)
         super().__init__(**kwargs)
 
-    def add_simple_option(self, name, type=None, default=None, help=None):
-        help_text = str(help)
-        if default is not None:
-            help_text += f" (default {default})"
-        return self.add_argument(
-            f"--{name}",
-            dest=name.replace("-", "_"),
-            type=type,
-            default=default,
-            help=help_text,
-        )
-
 
 parser = ArgParser(
     description=__doc__,
@@ -26,7 +14,7 @@ parser = ArgParser(
 )
 
 parser.add_argument(
-    "input",
+    "input_file",
     help="input file to the program",
 )
 
@@ -56,16 +44,19 @@ def three_sum(input, target_sum):
     return None
 
 
-if __name__ == "__main__":
-    input_file = cfg.input
-    input_values = []
+def read_input(input_file):
+    output = []
     with open(input_file) as file:
         for line in file:
-            input_values.append(int(line))
+            output.append(int(line))
+    return output
+
+
+if __name__ == "__main__":
+    input_values = read_input(cfg.input_file)
 
     num_1, num_2 = two_sum(input_values, 2020)
     print(num_1 * num_2)
 
     num_1, num_2, num_3 = three_sum(input_values, 2020)
     print(num_1 * num_2 * num_3)
-    print("done")
