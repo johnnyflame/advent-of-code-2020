8b424df790ef3feafb843a1219cb0e40d70fffaf
johnnyflame <johnnyflamelee@gmail.com>
2020-12-12 14:55:14 +0100

    solution for day 12 and test cases

--- src/day_12.py
+++ src/day_12.py
@@ -57,10 +57,79 @@ class Ship:
         return abs(curr_x - dest_x) + abs(curr_y - dest_y)
 
 
+class ShipUsingWayPoint:
+    def __init__(self, waypoint=(10, 1)):
+        self.coord = (0, 0)
+        self.waypoint = waypoint
+
+    def rotate_waypoint(self, degrees):
+
+        degrees = degrees % 360
+        wp_x, wp_y = self.waypoint
+        diff_x = self.coord[0] - wp_x
+        diff_y = self.coord[1] - wp_y
+        if degrees == 90:
+            self.waypoint = (self.coord[0] - diff_y, self.coord[1] + diff_x)
+        elif degrees == 180:
+            self.waypoint = (self.coord[0] + diff_x, self.coord[1] + diff_y)
+        elif degrees == 270:
+            self.waypoint = (self.coord[0] + diff_y, self.coord[1] - diff_x)
+
+    def move_ship_to_way_point(self, unit):
+        # move the ship
+        curr_x, curr_y = self.coord
+        wp_x, wp_y = self.waypoint
+        diff_x = wp_x - curr_x
+        diff_y = wp_y - curr_y
+        self.coord = (
+            curr_x + (diff_x * unit),
+            curr_y + (diff_y * unit),
+        )
+        # update the way point
+        self.waypoint = (self.coord[0] + diff_x, self.coord[1] + diff_y)
+
+    def move_waypoint(self, direction, magnitude):
+        curr_x, curr_y = self.waypoint
+        if direction == "N":
+            self.waypoint = (curr_x, curr_y + magnitude)
+        elif direction == "S":
+            self.waypoint = (curr_x, curr_y - magnitude)
+        elif direction == "E":
+            self.waypoint = (curr_x + magnitude, curr_y)
+        elif direction == "W":
+            self.waypoint = (curr_x - magnitude, curr_y)
+        elif direction == "R":
+            self.rotate_waypoint(magnitude)
+        elif direction == "L":
+            self.rotate_waypoint(-magnitude)
+        else:
+            raise ValueError(
+                f"Aceept directions in N,S,E,W and F, received {direction}"
+            )
+
+    def perform_next_action(self, s):
+        action = s[0]
+        value = int(s[1:])
+
+        if action == "F":
+            self.move_ship_to_way_point(value)
+        else:
+            self.move_waypoint(action, value)
+
+    def manhatten_distance(self, dest_x, dest_y):
+        curr_x, curr_y = self.coord
+        return abs(curr_x - dest_x) + abs(curr_y - dest_y)
+
+
 if __name__ == "__main__":
-    input_data = get_data(day=12)
+    actions = get_data(day=12).splitlines()
     ship = Ship()
-    for action in input_data.splitlines():
+    for action in actions:
         ship.perform_next_action(action)
 
     print(f"part A: {ship.manhatten_distance(0,0)}")
+
+    ship = ShipUsingWayPoint()
+    for action in actions:
+        ship.perform_next_action(action)
+    print(f"part B: {ship.manhatten_distance(0,0)}")
--- tests/day_12_test.py
+++ tests/day_12_test.py
@@ -1,9 +1,9 @@
 import pytest
-from src.day_12 import Ship
+from src.day_12 import Ship, ShipUsingWayPoint
 
 
 class Test_day_12:
-    def test_ship_move_simple_directions(self):
+    def test_simple_ship_movement(self):
         input_data = """F10
 N3
 F7
@@ -15,3 +15,35 @@ F11
             ship.perform_next_action(action)
         assert ship.coord == (17, -8)
         assert ship.manhatten_distance(0, 0)
+
+    def test_waypoint(self):
+        ship = ShipUsingWayPoint()
+        ship.perform_next_action("F10")
+        assert ship.coord == (100, 10)
+        assert ship.waypoint == (110, 11)
+        ship.perform_next_action("N3")
+        assert ship.coord == (100, 10)
+        assert ship.waypoint == (110, 14)
+        ship.perform_next_action("F7")
+        assert ship.coord == (170, 38)
+        assert ship.waypoint == (180, 42)
+        ship.perform_next_action("R90")
+        assert ship.coord == (170, 38)
+        assert ship.waypoint == (174, 28)
+        ship.perform_next_action("F11")
+        assert ship.coord == (214, -72)
+        assert ship.waypoint == (218, -82)
+
+    def test_rotation(self):
+        ship = ShipUsingWayPoint(waypoint=(1, 4))
+        ship.perform_next_action("R180")
+        assert ship.waypoint == (-1, -4)
+        ship.perform_next_action("R180")
+        assert ship.waypoint == (1, 4)
+
+        ship.perform_next_action("R90")
+        assert ship.waypoint == (4, -1)
+        ship.perform_next_action("R90")
+        assert ship.waypoint == (-1, -4)
+        ship.perform_next_action("R90")
+        assert ship.waypoint == (-4, 1)
